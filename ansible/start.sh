b9788cc95bad297eabeb42354c5054c8674dfb9f
karosuwindam <karosuwindam@gmail.com>
2021-06-04 19:02:22 +0900

    ???

--- ansible/start.sh
+++ ansible/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ansible-playbook -i inventory/web.yml playbook.yml --ask-become-pass
-kubectl apply -f docker-registry/namespace/
-kubectl apply -f docker-registry/pod/
+# kubectl apply -f docker-registry/namespace/
+# kubectl apply -f docker-registry/pod/
